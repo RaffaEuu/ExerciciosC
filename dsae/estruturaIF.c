3376f7b1a20025d0d3776d8bdd4d1d5311cd9259
Guilherme Casagrande <thegui4000@gmail.com>
2023-03-17 21:43:01 -0300

    incluindo

--- dsae/estruturaIF.c
+++ dsae/estruturaIF.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[]){
+int main(){
+    int num1, num2, num3;
+    printf("digite o primeiro numero inteiro: ");
+    scanf("%d", &num1);
+    printf("digite o primeiro numero inteiro: ");
+    scanf("%d", &num2);
+    printf("digite o primeiro numero inteiro: ");
+    scanf("%d", &num3);
+
+    if (num1 > num2 && num1 > num3){
+        printf("O %d é o maior", num1);
+    }else if(num2 > num1 && num2 > num3)
+    {
+        printf("O %d é o maior", num2);
+    }else{
+
+        printf("O %d é o maior", num3);
+    }
+    
+    
     return 0;
 }
