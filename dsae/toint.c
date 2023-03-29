630f19d2cc016d6bd4c45aac81c349e958994721
Guilherme Casagrande <thegui4000@gmail.com>
2023-03-29 19:33:59 -0300

    para int

---
+++ dsae/toint.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float myfloat = 42.8;
+    int myint;
+    printf("float is %f\n", myfloat);
+    myint = (int)myfloat;
+    printf("int is %d\n", myint);
+    return 0;
+}
