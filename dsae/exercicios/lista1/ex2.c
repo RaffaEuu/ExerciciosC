f060709499582feca6b399460ebba0086e7e18cf
Guilherme Casagrande <thegui4000@gmail.com>
2023-03-30 19:36:23 -0300

    ex2

--- dsae/exercicios/lista1/ex2.c
+++ dsae/exercicios/lista1/ex2.c
@@ -2,18 +2,37 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+int potenciacao(int base,int potencia){
+    int multiplicação = base;
+    for (size_t i = 1; i < potencia; i++){
+        multiplicação = multiplicação * base;
+    }
+    return multiplicação;
+}
+
+
+
 main(){
-    int cA, cB, cC, delta, menos, mais, rdelta;
+    float cA, cB, cC, delta, rdelta, maior, menor;
     printf("Digite o coeficiente A: ");
-    scanf("%d", &cA);
+    scanf("%f", &cA);
     printf("Digite o coeficiente B: ");
-    scanf("%d", &cB);
+    scanf("%f", &cB);
     printf("Digite o coeficiente C: ");
-    scanf("%d", &cC);
+    scanf("%f", &cC);
     
-    delta = pow(cB, 2) - 4 * cA * cC;
+    delta = pow (cB, 2) - 4 * cA * cC;
+
+    if(delta < 0 || cA == 0){
+        printf("\n  Esta equacao não possui raizes reais");
+        exit(1);
+    }
 
     rdelta = sqrt(delta);
 
-    printf("%d", rdelta);
+    maior = (-cB + rdelta) / 2 * cA;
+    menor = (-cB - rdelta) / 2 * cA;
+    printf("X1 = %.4f \nX2 = %.4f \n", maior, menor);
+    return 0;
 }
